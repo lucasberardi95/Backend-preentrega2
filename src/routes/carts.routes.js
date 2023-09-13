797816ed0a278b2399dabb04aa8abefeeebc8e9c
Lucas Berardi <lucas-l-@live.com>
2023-09-13 14:12:50 -0300

    Endpoint delete product from cart

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -5,35 +5,35 @@ import mongoose from "mongoose"
 
 const cartRouter = Router()
 
-cartRouter.get('/', async (req,res)=>{
+cartRouter.get('/', async (req, res) => {
     try {
         const carts = await cartModel.find()
-        res.status(200).send({result: 'OK', message: carts})
+        res.status(200).send({ result: 'OK', message: carts })
     } catch (error) {
-        res.status(400).send({error: `Error displaying carts:  ${error}`})
-    
+        res.status(400).send({ error: `Error displaying carts:  ${error}` })
+
     }
 })
 
-cartRouter.get('/:id', async (req,res)=>{
-    const {id} = req.params
+cartRouter.get('/:id', async (req, res) => {
+    const { id } = req.params
     try {
         const cart = await cartModel.findById(id)
-        res.status(200).send({result: 'OK', message: cart})
+        res.status(200).send({ result: 'OK', message: cart })
     } catch (error) {
-        res.status(400).send({error: `Id cart not found:  ${error}`})
-    
+        res.status(400).send({ error: `Id cart not found:  ${error}` })
+
     }
 })
 
 cartRouter.post('/', async (req, res) => {
     const response = await cartModel.create(req.body)
     try {
-        res.status(200).send({result: 'OK', message: response})
+        res.status(200).send({ result: 'OK', message: response })
     } catch (error) {
-        res.status(400).send({error: `Cart already exist: ${error}`})
+        res.status(400).send({ error: `Cart already exist: ${error}` })
     }
-    
+
 })
 
 cartRouter.put('/:cid/product/:pid', async (req, res) => {
@@ -47,17 +47,40 @@ cartRouter.put('/:cid/product/:pid', async (req, res) => {
             res.status(200).send({ result: 'OK', message: response })
         }
     } catch (error) {
-        res.status(400).send({error: `Error adding product: ${error}`})
+        res.status(400).send({ error: `Error adding product: ${error}` })
     }
 })
 
+cartRouter.delete('/:cid/products/:pid', async (req, res) => {
+    const { cid, pid } = req.params
+    try {
+        const cart = await cartModel.findById(cid)
+        if (cart) {
+            const productIndex = cart.products.findIndex(prod => prod.id_prod.equals(new mongoose.Types.ObjectId(pid)))
+            let deletedProduct
+			if (productIndex !== -1) {
+				deletedProduct = cart.products[productIndex]
+				cart.products.splice(productIndex, 1)
+			} else {
+				res.status(404).send({ result: 'Id Product Not Found', message: cart })
+			}
+			await cart.save()
+			res.status(200).send({ result: 'OK', message: deletedProduct })
+		} else {
+			res.status(404).send({ result: 'Cart Not Found', message: cart })
+		}
+	} catch (error) {
+		res.status(400).send({ error: `Error deleting product: ${error}` })
+	}
+})
+
 cartRouter.delete('/:id', async (req, res) => {
-    const {id} = req.params
-    const response = await cartModel.deleteOne({_id: id})
+    const { id } = req.params
+    const response = await cartModel.deleteOne({ _id: id })
     if (response)
-        res.status(200).send({result: 'OK ', message: response})
+        res.status(200).send({ result: 'OK ', message: response })
     else
-        res.status(404).send({error: `Id cart not found: ${error}`})
+        res.status(404).send({ error: `Id cart not found: ${error}` })
 })
 
 export default cartRouter
